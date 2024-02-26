2b61821f49bd67ac4487f4ccc4728fef261efe80
Ankit Bajpai <ankit65bajpai@gmail.com>
2024-02-26 17:44:14 +0530

    Fix hero and highlight mobile view and added links in footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Twitter, Linkedin } from 'lucide-react';
 import "./Footer.css";
 
 const Footer = () => {
@@ -11,8 +10,12 @@ const Footer = () => {
                         <img src="/logo_white.png" alt="" />
                     </span>
                     <span className="socialMediaIcons">
-                        <img src="/TwitterX.png" alt="twitterX_icon" />
-                        <img src="/telegram.png" alt="telegram_icon" />
+                        <a href="https://twitter.com/BITBhoomi" target="_blank">
+                            <img src="/TwitterX.png" alt="twitterX_icon" />
+                        </a>
+                        <a href="https://t.co/qMVAgna0Xl" target="_blank">
+                            <img src="/telegram.png" alt="telegram_icon" />
+                        </a>
                     </span>
                 </div>
                 <p>© 2024 Beyond Imagination Technologies Pvt. Ltd.</p>
