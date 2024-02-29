1c074a7d89c223bf101bf7ef0073009bd0042485
Ankit Bajpai <ankit65bajpai@gmail.com>
2024-02-29 20:42:27 +0530

    Updated tokenomics doc

--- src/Components/Home/Tokenomics/Tokenomics.jsx
+++ src/Components/Home/Tokenomics/Tokenomics.jsx
@@ -1,14 +1,25 @@
 import './Tokenomics.css';
 
 const Tokenomics = () => {
+    const handleDownload = () => {
+        const filePath = 'https://drive.google.com/file/d/1KcwtSl2sZ35qZ_LpsGPHie1tpbnIFdg8/view?usp=sharing';
+        const link = document.createElement('a');
+        link.href = filePath;
+        link.target = "_blank";
+        link.setAttribute('download', 'BitBhoomi White Paper.pdf');
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+    }
+
     return (
         <>
             <section className="tokenomicsSection">
                 <h1 className="heading">Tokenomics</h1>
-                <div className="tokenomicsDoc">
+                <div className="document">
                     <img src="/tokenomics.png" alt="" />
                 </div>
-                <div className="downloadBtnDiv">
+                <div className="downloadBtnDiv" onClick={handleDownload}>
                     <button className="downloadBtn">
                         Download
                         <img src="/download.png" alt="download_icon" />
